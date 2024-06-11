d989dba38c7bd194caff2697d64648d9868856ad
Bach <tahatranzzz@gmail.com>
2024-06-11 10:55:18 +0700

    sfx+test

--- Assets/Script/Lose.cs
+++ Assets/Script/Lose.cs
@@ -12,7 +12,7 @@ public class Lose : MonoBehaviour
     }
     public void ReplayLoseBtn()
     {
-        Debug.Log("aaaa");
+        // Debug.Log("aaaa");
         SceneManager.LoadScene(0);
         // var rect = losePopup.GetComponent<RectTransform>();
         // rect.DOAnchorPos(new Vector2(rect.anchoredPosition.x, 2000), 0.5f)
